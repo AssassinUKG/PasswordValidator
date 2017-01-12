b8aef850d7ff0efc6ea3430a88ce862041970c1a
AssassinUKG <AssassinUKG@ASSASSIN>
2017-01-12 16:04:35 +0000

    first

--- PasswordValidator/PasswordChecker.cs
+++ PasswordValidator/PasswordChecker.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace PasswordValidator
 {
     internal static class PasswordChecker
-    {
+    { // https://github.com/lsauer/entropy/blob/master/shantropy/Main.cs  need to achieve better (real) entropy
         private const string lettersLower = "abcdefghijklmnopqrstuvwxyz";
         private const string lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         private const string specialChars = "~`!@#$%^&*()-_+=";
@@ -14,7 +15,7 @@ namespace PasswordValidator
         private static readonly int UpperChars = lettersUpper.Length;
         private static readonly int SpecialChars = specialChars.Length;
         private static readonly int Digits = numbers.Length;
-        private static readonly int otherchars = TotalChars - (LowerChars + UpperChars + SpecialChars + Digits);
+        private static  int otherchars;
         private static int CharSet = 0;
 
         private static int CaculateBits(string password)
@@ -24,6 +25,7 @@ namespace PasswordValidator
             bool p_lettersLower = false, p_lettersUpper = false, p_specialChars = false, p_numbers = false, p_other = false;
 
             Char chr;
+            otherchars = TotalChars - (LowerChars + UpperChars + SpecialChars + Digits);
 
             for (int i = 0; i < password.Length - 1; i++)
             {
@@ -59,9 +61,9 @@ namespace PasswordValidator
             {
                 CharSet = CharSet + otherchars;
             }
-
+            Debug.Print(CharSet.ToString());
             double bits = Math.Log(CharSet) * (password.Length / Math.Log(2));
-            return (int)bits;
+            return (int)Math.Floor(bits);
         }
 
         public static int EvalPwdStrength(string password)
